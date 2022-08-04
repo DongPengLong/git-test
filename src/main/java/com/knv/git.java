91ecd194656e7c7574289dadabea7a6af6f3a097
knv <1308764814@qq.com>
2022-08-04 11:36:21 +0800

    push commit 2

--- src/main/java/com/knv/git.java
+++ src/main/java/com/knv/git.java
@@ -9,5 +9,6 @@ public class git {
         System.out.println("hot-fix Test");
         System.out.println("hot-fix Test");
         System.out.println("GitHub Push");
+        System.out.println("GitHub Push 2");
     }
 }
